5169f0fb846ff806bf0f48fc4b1eb996836b3bb4
kennbeckloff <kennbeckloff688@gmail.com>
2022-08-22 11:15:41 +0300

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+  # write code 
+  attr_accessor :side1, :hypotenuse, :adjacent
+  def initialize(side1, hypotenuse, adjacent)
+    @side1 = side1
+    @hypotenuse = hypotenuse
+    @adjacent = adjacent
+  end
+  def kind 
+    if self.adjacent <= 0 || self.side1 <= 0 || self.hypotenuse <= 0
+      raise TriangleError
+      elsif self.adjacent == self.hypotenuse && self.hypotenuse == self.side1
+      :equilateral
+    elsif self.adjacent == self.hypotenuse || self.hypotenuse == self.side1 || self.side1 == self.adjacent
+      :isosceles
+    elsif self.adjacent != self.hypotenuse && self.hypotenuse != self.side1 && self.side1 != self.adjacent
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError 
+    def message
+      "Not a valid triangle"
+
+    end
+  end 
+  class TriangleError < StandardError
+    def message
+      "Triangle inequality failed"
+    end
+  end
+  
 end
